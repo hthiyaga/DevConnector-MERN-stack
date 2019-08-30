cf34722261c0a3a93f1ad4e92f17c30252719f3d
hthiyaga <hthiyaga@cs.odu.edu>
2019-08-30 18:51:04 -0400

    User authentication/Login

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 const auth = require('../../middleware/auth');
 
 const User = require('../../models/User');
@@ -15,5 +19,61 @@ router.get('/', auth, async (req, res) => {
     res.status(500).send('server error');
   }
 });
+
+//@route  POST api/auth
+//@desc   Authenticate User and Get token
+//@access Public
+router.post(
+  '/',
+  [
+    check('email', 'Please enter a valid email').isEmail(),
+    check('password', 'Password is required').exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    console.log(req.body);
+    const { email, password } = req.body;
+
+    try {
+      //see if user exists and match with the password
+
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({ errors: [{ msg: 'Invalid Creds' }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({ errors: [{ msg: 'Invalid Creds' }] });
+      }
+
+      //Return Jsonwebtoken
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+      //res.send('User Registered');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('server error');
+    }
+  }
+);
 
 module.exports = router;
