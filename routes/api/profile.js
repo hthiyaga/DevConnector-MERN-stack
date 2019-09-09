2f9ce1d654e94feea450f4511cf5cc627d2a5d7b
hthiyaga <hthiyaga@cs.odu.edu>
2019-09-09 16:50:00 -0400

    Delete profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -158,4 +158,24 @@ router.get('/user/:user_id', async (req, res) => {
     res.status(500).send('Server error');
   }
 });
+
+//@route  DELETE api/profile
+//@desc   Delete profile, users and posts
+//@access private
+
+router.delete('/', auth, async (req, res) => {
+  try {
+    //@todo remove users posts
+    //remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    //remove User
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: 'User Removed' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
 module.exports = router;
