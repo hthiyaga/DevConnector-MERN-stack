8214bd8e181fe30ea76e9a63762cd33394cf675a
hthiyaga <hthiyaga@cs.odu.edu>
2019-09-24 23:23:43 -0400

    Like a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -112,4 +112,31 @@ router.delete('/:id', auth, async (req, res) => {
   }
 });
 
+//@route  POST api/posts/like/:id
+//@desc   Like a post
+//@access Private
+
+router.put('/like/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    //check if user liked the post
+
+    if (
+      post.likes.filter(like => like.user.toString() === req.user.id).length > 0
+    ) {
+      return res.status(400).json({ msg: 'Post has been already liked' });
+    }
+
+    post.likes.unshift({ user: req.user.id });
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('server error');
+  }
+});
+
 module.exports = router;
