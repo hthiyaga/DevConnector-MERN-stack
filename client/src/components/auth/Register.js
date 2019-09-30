2f52830ce5ac7d4593391e1797adb9eea2e779c2
hthiyaga <hthiyaga@cs.odu.edu>
2019-09-30 12:43:31 -0400

    Alert component and action call

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,12 @@
 import React, { Fragment, useState } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
+
 //import axios from 'axios';
 
-const Register = () => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -16,7 +20,7 @@ const Register = () => {
   const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
-      console.log('Password do not match');
+      setAlert('Password do not match', 'danger');
     } else {
       // sending data to backend - one way
       //   const newUser = {
@@ -105,4 +109,10 @@ const Register = () => {
   );
 };
 
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired
+};
+export default connect(
+  null,
+  { setAlert }
+)(Register);
