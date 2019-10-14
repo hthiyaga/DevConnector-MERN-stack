488b3fb76d95dcd67ab7f407b97e4b0314cee6c2
hthiyaga <hthiyaga@cs.odu.edu>
2019-10-14 11:53:44 -0400

    Displaying Github repos

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -63,7 +63,7 @@ export const getProfileById = userId => async dispatch => {
   }
 };
 //get all profiles
-export const gerGithubRepos = username => async dispatch => {
+export const getGithubRepos = username => async dispatch => {
   try {
     const res = await axios.get(`/api/profile/github/${username}`);
 
