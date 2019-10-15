7a6b3ab694c12606d64bf2a4d7ea42fd5a3c21ce
hthiyaga <hthiyaga@cs.odu.edu>
2019-10-15 10:17:24 -0400

    Deployment changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
+const path = require('path');
 const app = express();
 
 //Connect Database
@@ -9,7 +9,7 @@ connectDB();
 //Init middleware
 app.use(express.json({ extend: false }));
 
-app.get('/', (req, res) => res.send('API Running'));
+//app.get('/', (req, res) => res.send('API Running'));
 
 //Define Routes
 app.use('/api/users', require('./routes/api/users'));
@@ -17,6 +17,17 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+// Server static assets in production
+
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server start on port ${PORT}`));
